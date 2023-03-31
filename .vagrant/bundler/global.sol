6ffd47472b36ee93fe06833339ed92247079c4bf
Yago Alexandre <77808082+goltaraya@users.noreply.github.com>
2023-03-31 11:48:45 -0300

    Delete .vagrant directory

--- .vagrant/bundler/global.sol
+++
@@ -1,1 +0,0 @@
-{"dependencies":[["log4r",[">= 0"]],["concurrent-ruby",["~> 1.0"]],["i18n",[">= 0"]],["micromachine",[">= 2","< 4"]],["vagrant-vbguest",["= 0.21.0"]],["vagrant-winnfsd",["= 1.4.0"]]],"checksum":"93e81cff2a8602e75f80a999ba8ad89da6dc06b226725c4f194222c7cb852c2a","vagrant_version":"2.3.4"}
